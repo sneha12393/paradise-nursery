645e4361f2dfc23862efac71f5addd730b887eda
sneha12393 <snehapeswani01@gmail.com>
2026-09-10 15:42:12 +0530

    Add AboutUs component

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,20 @@
+ <p>
+        Paradise Nursery is an online plant shop that offers a wide variety
+        of beautiful and healthy houseplants.
+      </p>
+
+      <p>
+        Our goal is to make it easy for plant lovers to discover, choose,
+        and purchase plants for their homes and offices.
+      </p>
+
+      <p>
+        We provide plants across different categories, along with convenient
+        shopping cart functionality for a simple and enjoyable shopping
+        experience.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
