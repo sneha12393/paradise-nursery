0058e6464fba96cb356cd8420de9848a70ea1c34
sneha12393 <snehapeswani01@gmail.com>
2026-09-10 15:51:49 +0530

    Add Paradise Nursery landing page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1>Paradise Nursery</h1>
+
+        <p>
+          Welcome to Paradise Nursery, your destination for beautiful
+          and healthy houseplants.
+        </p>
+
+        <button className="get-started">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
